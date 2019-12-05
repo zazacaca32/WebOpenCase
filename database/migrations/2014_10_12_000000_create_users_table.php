31d713108ab51aadbec9186e36d34483939a92f3
zazacaca32 <filippof3@ya.ru>
2019-12-05 22:01:37 +0300

    Change migration

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,10 +15,9 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->bigIncrements('id');
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
+            $table->string('username');
+            $table->bigInteger('steamid');
+            $table->text('avatar');
             $table->rememberToken();
             $table->timestamps();
         });
